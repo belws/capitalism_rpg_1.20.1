7734a71d9e8dc64f5c4ae7b990ba9b1a32c29ec3
Bogdan <4bogdanalexandru@gmail.com>
2026-09-01 01:06:21 +0300

    add/render method It now renders the phone inside the game at the right place for my window size and this will be worked on later Also I had to crop the png for this

--- src/main/java/github/belws/crpg/item/custom/helper/PhoneOverlay.java
+++ src/main/java/github/belws/crpg/item/custom/helper/PhoneOverlay.java
@@ -1,6 +1,9 @@
 package github.belws.crpg.item.custom.helper;
 
 import github.belws.crpg.CapitalismRpg;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RenderGuiEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -12,8 +15,24 @@ import net.minecraftforge.fml.common.Mod;
 )
 public class PhoneOverlay {
 
+    private static final ResourceLocation PHONE_TEXTURE =
+            new ResourceLocation(CapitalismRpg.MOD_ID, "gui/phone_sprite_demo_0.png");
+
     @SubscribeEvent
     public static void renderPhone(RenderGuiEvent.Post event){
+        GuiGraphics guiGraphics = event.getGuiGraphics();
 
+        //This is temporary until i figure out how to make it stay in its place:)
+        guiGraphics.blit(
+                PHONE_TEXTURE,
+                650,
+                300,
+                0,
+                0,
+                256,
+                256,
+                256,
+                256
+        );
     }
 }
