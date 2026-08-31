56d16f4901a140c0e88f492a1804d75b9620f506
Bogdan <4bogdanalexandru@gmail.com>
2026-09-01 00:42:24 +0300

    setup/structure for rendering png

---
+++ src/main/java/github/belws/crpg/item/custom/PhoneItem.java
@@ -0,0 +1,12 @@
+package github.belws.crpg.item.custom;
+
+import net.minecraft.world.item.Item;
+
+import java.util.Properties;
+
+public class PhoneItem extends Item {
+    public PhoneItem(Properties pProperties){
+        super(pProperties);
+    }
+
+}
---
+++ src/main/java/github/belws/crpg/item/custom/helper/PhoneOverlay.java
@@ -0,0 +1,12 @@
+package github.belws.crpg.item.custom.helper;
+
+import github.belws.crpg.CapitalismRpg;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(
+        modid = CapitalismRpg.MOD_ID,
+        value = Dist.CLIENT
+)
+public class PhoneOverlay {
+}
