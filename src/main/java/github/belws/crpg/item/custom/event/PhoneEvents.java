a570e19e7affc492688361e06d0b834b22faf967
Bogdan <4bogdanalexandru@gmail.com>
2026-09-01 06:52:28 +0300

    feat/onAttack event canceled while holding the phone

---
+++ src/main/java/github/belws/crpg/item/custom/event/PhoneEvents.java
@@ -0,0 +1,23 @@
+package github.belws.crpg.item.custom.event;
+
+import github.belws.crpg.CapitalismRpg;
+import github.belws.crpg.item.ModItems;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(
+        modid = CapitalismRpg.MOD_ID,
+        value = Dist.CLIENT
+)
+public class PhoneEvents {
+
+    @SubscribeEvent
+    //This will prevent player from being able to deal damage while holding the phone
+    public static void onAttackEntity(AttackEntityEvent event){
+        if (event.getEntity().getMainHandItem().is(ModItems.PHONE.get())){
+            event.setCanceled(true);
+        }
+    }
+}
