8d91af2e4bc465aea8ee60cc49dfe163362454df
Bogdan <4bogdanalexandru@gmail.com>
2026-09-01 07:32:14 +0300

    feat/onLeftClickBlock event canceled while holding the phone This will now cancel all interactions while holding the phone and we can focus on implementing phone behaviour

--- src/main/java/github/belws/crpg/item/custom/event/PhoneEvents.java
+++ src/main/java/github/belws/crpg/item/custom/event/PhoneEvents.java
@@ -4,6 +4,7 @@ import github.belws.crpg.CapitalismRpg;
 import github.belws.crpg.item.ModItems;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -17,6 +18,12 @@ public class PhoneEvents {
     //This will prevent player from being able to deal damage while holding the phone
     public static void onAttackEntity(AttackEntityEvent event){
         if (event.getEntity().getMainHandItem().is(ModItems.PHONE.get())){
+            event.setCanceled(true);
+        }
+    }
+    @SubscribeEvent
+    public static void onLeftClickBlock(PlayerInteractEvent.LeftClickBlock event){
+        if(event.getEntity().getMainHandItem().is(ModItems.PHONE.get())) {
             event.setCanceled(true);
         }
     }
