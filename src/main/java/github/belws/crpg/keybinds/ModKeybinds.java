55fd7de70be8755ea28bd8c94599454c4e0e584b
Bogdan <4bogdanalexandru@gmail.com>
2026-09-05 12:31:59 +0300

    add/map keybinds

---
+++ src/main/java/github/belws/crpg/keybinds/ModKeybinds.java
@@ -0,0 +1,40 @@
+package github.belws.crpg.keybinds;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import org.lwjgl.glfw.GLFW;
+
+public class ModKeybinds {
+    public static final KeyMapping PHONE_UP = new KeyMapping(
+            "key.capitalismrpg.phone_up",
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_UP,
+            "key.categories.capitalismrpg"
+    );
+
+    public static final KeyMapping PHONE_DOWN = new KeyMapping(
+            "key.capitalismrpg.phone_down",
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_DOWN,
+            "key.categories.capitalismrpg"
+    );
+
+    public static final KeyMapping PHONE_LEFT = new KeyMapping(
+            "key.capitalismrpg.phone_left",
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_LEFT,
+            "key.categories.capitalismrpg"
+    );
+
+    public static final KeyMapping PHONE_RIGHT = new KeyMapping(
+            "key.capitalismrpg.phone_right",
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_RIGHT,
+            "key.categories.capitalismrpg"
+    );
+
+    public static void register() {
+
+    }
+
+}
