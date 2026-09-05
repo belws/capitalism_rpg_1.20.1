cd7c280aeee4f49bca7b389b5864d5f011bfcafb
Bogdan <4bogdanalexandru@gmail.com>
2026-09-05 12:23:43 +0300

    add/register keybinds

---
+++ src/main/java/github/belws/crpg/handlers/ModClientEvents.java
@@ -0,0 +1,21 @@
+package github.belws.crpg.handlers;
+
+import github.belws.crpg.keybinds.ModKeybinds;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(
+        modid = "capitalismrpg",
+        bus = Mod.EventBusSubscriber.Bus.MOD
+)
+
+public class ModClientEvents {
+    @SubscribeEvent
+    public static void registerKeyMappings(RegisterKeyMappingsEvent event) {
+        event.register(ModKeybinds.PHONE_UP);
+        event.register(ModKeybinds.PHONE_DOWN);
+        event.register(ModKeybinds.PHONE_LEFT);
+        event.register(ModKeybinds.PHONE_RIGHT);
+    }
+}
