eaf4c7ccb7638e31e342f019a877b0747aa4d751
Bogdan <4bogdanalexandru@gmail.com>
2026-09-06 21:16:31 +0300

    refactor apps in their own classes

--- src/main/java/github/belws/crpg/apps/NpcManagerApp.java
+++ src/main/java/github/belws/crpg/apps/NpcManagerApp.java
@@ -1,4 +1,22 @@
 package github.belws.crpg.apps;
 
-public class NpcManagerApp {
+import github.belws.crpg.CapitalismRpg;
+import net.minecraft.resources.ResourceLocation;
+import org.jline.utils.Log;
+
+public class NpcManagerApp extends App {
+    public NpcManagerApp() {
+        super(
+                "NPC Manager",
+                new ResourceLocation(
+                        CapitalismRpg.MOD_ID,
+                        "gui/apps/app2-npc-manager.png"
+                )
+        );
+    }
+
+    @Override
+    public void open() {
+        Log.info("Opening NPC Manager App");
+    }
 }
