4cdf878e85823d00db76bc899ff306da5dd09130
Bogdan <4bogdanalexandru@gmail.com>
2026-09-08 11:38:27 +0300

    add/phone controller class to refactor resposability

--- src/main/java/github/belws/crpg/item/custom/phone/helper/PhoneController.java
+++ src/main/java/github/belws/crpg/item/custom/phone/helper/PhoneController.java
@@ -1,4 +1,19 @@
 package github.belws.crpg.item.custom.phone.helper;
 
+import github.belws.crpg.item.custom.phone.apps.App;
+
 public class PhoneController {
+    private static App activeApp = null;
+
+    public static App getActiveApp() {
+        return activeApp;
+    }
+
+    public static void openApp(App app) {
+        activeApp = app;
+    }
+
+    public static void closeApp() {
+        activeApp = null;
+    }
 }
