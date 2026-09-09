38969c46df3d556365f3795dab2f871de3cd84c3
Bogdan <4bogdanalexandru@gmail.com>
2026-09-09 13:18:40 +0300

    add/input handler class to handle inputs

---
+++ src/main/java/github/belws/crpg/item/custom/phone/event/PhoneInputHandler.java
@@ -0,0 +1,58 @@
+package github.belws.crpg.item.custom.phone.event;
+
+import github.belws.crpg.CapitalismRpg;
+import github.belws.crpg.item.ModItems;
+import github.belws.crpg.item.custom.phone.helper.PhoneAction;
+import github.belws.crpg.item.custom.phone.helper.PhoneController;
+import github.belws.crpg.keybinds.ModKeybinds;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(
+        modid = CapitalismRpg.MOD_ID,
+        value = Dist.CLIENT
+)
+public class PhoneInputHandler {
+
+    @SubscribeEvent
+    public static void onClickTick(TickEvent.ClientTickEvent event) {
+        if (event.phase != TickEvent.Phase.END) {
+            return;
+        }
+
+        Minecraft minecraft = Minecraft.getInstance();
+
+        boolean canUsePhone =
+                minecraft.player != null
+                && minecraft.screen == null
+                && minecraft.isWindowActive()
+                && minecraft.player.getMainHandItem().is(ModItems.PHONE.get());
+
+        consumeAction(ModKeybinds.PHONE_LEFT, PhoneAction.LEFT, canUsePhone);
+        consumeAction(ModKeybinds.PHONE_RIGHT, PhoneAction.RIGHT, canUsePhone);
+        consumeAction(ModKeybinds.PHONE_UP, PhoneAction.UP, canUsePhone);
+        consumeAction(ModKeybinds.PHONE_DOWN, PhoneAction.DOWN, canUsePhone);
+        consumeAction(ModKeybinds.PHONE_CONFIRM, PhoneAction.CONFIRM, canUsePhone);
+        consumeAction(ModKeybinds.APP_EXIT, PhoneAction.BACK, canUsePhone);
+    }
+
+    private static void consumeAction(
+            KeyMapping key,
+            PhoneAction action,
+            boolean canUsePhone
+    ) {
+        boolean pressed = false;
+
+        while (key.consumeClick()) {
+            pressed = true;
+        }
+
+        if (pressed && canUsePhone) {
+            PhoneController.handleAction(action);
+        }
+    }
+}
