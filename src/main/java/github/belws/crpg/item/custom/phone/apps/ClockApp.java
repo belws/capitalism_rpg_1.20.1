96dd01d32bea4ed85d02d7f1cd47ba2c0faecafd
Bogdan <4bogdanalexandru@gmail.com>
2026-09-10 23:52:52 +0300

    feat: add hour and minute to Clock app

--- src/main/java/github/belws/crpg/item/custom/phone/apps/ClockApp.java
+++ src/main/java/github/belws/crpg/item/custom/phone/apps/ClockApp.java
@@ -2,10 +2,15 @@ package github.belws.crpg.item.custom.phone.apps;
 
 import github.belws.crpg.CapitalismRpg;
 import github.belws.crpg.item.custom.phone.ui.ButtonElement;
+import github.belws.crpg.item.custom.phone.ui.NumberElement;
 import net.minecraft.resources.ResourceLocation;
 import org.jline.utils.Log;
 
 public class ClockApp extends App {
+
+    private final NumberElement hourElement;
+    private final NumberElement minuteElement;
+
     public ClockApp() {
         super(
                 "Clock",
@@ -18,10 +23,26 @@ public class ClockApp extends App {
                         "gui/clock-app-screen.png"
                 )
         );
+
         addElement(new ButtonElement(
                 70,180,110,20,
                 "Add Alarm",
                 () -> Log.info("Test Add Alarm Pressed")
         ));
+
+        hourElement = new NumberElement(
+                40, 100, 70, 20,
+                "Hour",
+                0, 23, 6
+        );
+
+        minuteElement = new NumberElement(
+                100, 100, 70, 20,
+                "Minute",
+                0,59,0
+        );
+
+        addElement(hourElement);
+        addElement(minuteElement);
     }
 }
